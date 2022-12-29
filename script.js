3be41342ab7358494e935b2dc7c8e495d22961b5
Ilhom Abdulahatov <abdulahatov03gmail.com>
2022-12-29 04:26:31 +0300

    added some examples

--- script.js
+++ script.js
@@ -306,7 +306,7 @@
 //     message = ''
 // }
 
-//ternary operator
+// ternary operator
 // login == 'Emplyee'
 //     ? message = 'hello'
 //     : login == 'Diroctor'
@@ -314,3 +314,89 @@
 //         : login == ''
 //             ? message = 'no login'
 //             : message = ''
+
+// OBJECT EXAMPLES
+//Examples
+// 1
+// let user ={name:'John', surName:'Smith'};
+// user.name='Pete';
+// delete user.name
+// console.log(user);
+
+// 2
+//  let salaries={
+//     John: 100,
+//     Ann:160,
+//     Pete:130
+//  }
+
+//  for (const key in salaries) {
+//     let sum = 0;
+//     sum+=salaries[key]
+
+//  }
+
+// let arrayIntegers = [1, 2, 3, 4, 5];
+// let arrayIntegers1 = arrayIntegers.slice(0, 2); // returns [1,2]
+// console.log(arrayIntegers,arrayIntegers1);
+
+// let arrayIntegersOriginal1 = [1, 2, 3, 4, 5];
+// let arrayIntegers1 = arrayIntegersOriginal1.splice(0, 2);
+// console.log(arrayIntegersOriginal1, arrayIntegers1);
+
+
+
+//Object examples
+// 1
+// let obj={
+//         id:1,
+//         name:"WebBrain"
+//     }
+//     for (const key in obj) {
+//        console.log(key);
+//     }
+
+// 2
+// let obj = {
+//     id: 1,
+//     name: "WebBrain",
+// };
+// for (const key in obj) {
+//     let value = obj[key];
+//     if (Number(value)) {
+//         console.log(key);
+//     }
+// }
+
+// 3
+// let obj = {
+//     id: 1,
+//     name: "WebBrain",
+//     offline: true,
+//     online: true,
+//     individual: false
+// }
+
+// for (const key in obj) {
+//     let value = obj[key];
+//     if (value === true || value === false) {
+//         console.log(key, obj[key]);
+//     }
+// }
+
+// 4
+// let obj = {
+//     id: 1,
+//     name: "WebBrain",
+//     offline: true,
+//     online: true,
+//     individual: false
+// }
+// function Search(obj, str) {
+//     for (const key in obj) {
+//         if (obj[key] == str){
+//             return `${key}: ${obj[key]}`
+//         }
+//     }
+// }
+// console.log(Search(obj, 'WebBrain'));
